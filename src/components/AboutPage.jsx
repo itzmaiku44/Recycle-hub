8e13cab9559c6c9eb197db1b9e3b31b432e9181a
Maykel <160039856+itzmaiku44@users.noreply.github.com>
2025-12-19 14:45:15 +0800

    Fix line breaks in AboutPage component

--- src/components/AboutPage.jsx
+++ src/components/AboutPage.jsx
@@ -23,11 +23,11 @@ const AboutPage = () => {
             bringing together routes, maps, and user insights in one friendly
             interface.
           </p>
-          <br></br>
+          <br />
           <p style={{ fontSize: '1.05rem', lineHeight: 1.7 }}>
-            This project is created by student of CIS2102 - WEB DEVELOPMENT 2 - Group6 S.Y. 2025.<br></br>
-            Lead Developer: Michael James Catubig<br></br>
-            Frontend Developer: Adam Werner T. Villa, Johndear M. Halili<br></br>
+            This project is created by student of CIS2102 - WEB DEVELOPMENT 2 - Group6 S.Y. 2025.<br />
+            Lead Developer: Michael James Catubig<br />
+            Frontend Developer: <br />Adam Werner T. Villa <br />Johndear M. Halili
           </p>
         </section>
       </main>
